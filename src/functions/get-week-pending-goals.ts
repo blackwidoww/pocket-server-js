71469fb1e1bacb81c41339bea03f86cf838972f3
romanoff <sabrinaflor09@gmail.com>
2024-09-11 11:07:47 -0300

    get-week-pending-goals

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -1,23 +1,42 @@
-import dayjs from "dayjs";
-import weekOfYear from "dayjs/plugin/weekOfYear";
-import { db } from "../db";
-import { and, lte, sql } from "drizzle-orm";
-import { goals } from "../db/schema";
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { and, count, gte, lte, sql } from 'drizzle-orm'
+import { goalCompletions, goals } from '../db/schema'
+//import { count } from 'console'
 // import { title } from "process";
 
 dayjs.extend(weekOfYear)
 
 export function getWeekPendingGoals() {
-    const lastDayOfWeek = dayjs().endOf('week').toDate()
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
 
-    const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
-        db.select({
-            id: goals.id,
-            title: goals.title,
-            desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
-            createdAt: goals.createdAt,
-        })
-        .from(goals).where(lte(goals.createdAt, lastDayOfWeek))
-        
-    )
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goals.createdAt, firstDayOfWeek),
+          lte(goals.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
 }
